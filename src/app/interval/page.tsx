95f3c79d59b6d425c210d996221d62d068307697
David Ly <davy803@gmail.com>
2024-01-15 01:59:31 -0600

    Fix build error

--- src/app/interval/page.tsx
+++ src/app/interval/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import { IntervalTest } from "@/components/component/interval-test";
+import { useScoreStore } from "@/lib/state/score-context";
 
 export default function Interval() {
+  const resetAll = useScoreStore((store) => store.resetAll);
+  
+  resetAll();
+  
   return <IntervalTest />;
 }
--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,8 +1,11 @@
+"use client";
+
 import { NoteTest } from "@/components/component/note-test";
 import { useScoreStore } from "@/lib/state/score-context";
 
 export default function Notes() {
   const resetAll = useScoreStore((store) => store.resetAll);
   resetAll();
+
   return <NoteTest />;
 }
